92cdc98ac8d3e4cc65fbe333f5c66da76045d451
MarkTBSS <marktbss@gmail.com>
2023-11-09 07:30:04 +0700

    [ADD] C File

---
+++ src/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char *s = "Life is beautiful";
+    printf("Hello, World!\n");
+    printf("%s\n", s);
+
+    char a[] = "Life is beautiful";
+    printf("Hello, World!\n");
+    printf("%s\n", a);
+    return 0;
+}
